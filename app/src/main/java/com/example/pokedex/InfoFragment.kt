01d10d91af2e0391b764d464bd306d3167ee1d6d
dantist987 <adik.isak2412@gmail.com>
2022-07-04 10:36:46 +0600

    5 and 6 homeworks

--- app/src/main/java/com/example/pokedex/InfoFragment.kt
+++ app/src/main/java/com/example/pokedex/InfoFragment.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pokedex.databinding.FragmentInfoBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 
 class InfoFragment : BottomSheetDialogFragment() {
 
+    lateinit var binding: FragmentInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding = FragmentInfoBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tag.toString()
     }
 }
